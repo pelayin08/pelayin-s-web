0321b39e31eacafd98caeee0499a143adffb4d86
pelayin08 <pelayin08@users.noreply.github.com>
2026-02-26 18:10:55 +0000

    Build for 68978029a6361048847662d5ac0cb5ac491b65ac

---
+++ generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COMMON_CONFIG="./assets/common_config.ini"
+
+declare -a GALLERY_CONFIGS=(
+    "config/Projects/test.ini"
+    "config/Biography/Biography.ini"
+)
+
+for config in "${GALLERY_CONFIGS[@]}"; do
+    python3 ./gallery_generator.py $COMMON_CONFIG $config
+done
